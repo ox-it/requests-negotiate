b32e8985220a0e700fb6b815aac3a7d0b475d60e
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2014-06-18 11:44:10 +0100

    Added license info (BSD)

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ from distutils.core import setup
 setup(
     name='requests-negotiate',
     version='1.0',
+    license='BSD',
     packages=['requests_negotiate'],
+    classifiers=['License :: OSI Approved :: BSD License',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Topic :: Internet :: WWW/HTTP :: Dynamic Content'],
     install_requires=['requests', 'python-gssapi'],
 )
