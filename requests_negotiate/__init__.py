5307cf805d408a956b17013170c719eeccfd362f
Robert Bradley <robert.bradley@it.ox.ac.uk>
2014-08-12 13:54:39 +0100

    Fix formatting to make it PEP8-compliant

--- requests_negotiate/__init__.py
+++ requests_negotiate/__init__.py
@@ -16,7 +16,9 @@ class HTTPNegotiateAuth(AuthBase):
         if self.preempt or host in self.contexts:
             self.contexts[host] = ctx = self.get_context(host)
             token = ctx.step(None)
-            request.headers['Authorization'] = 'Negotiate ' + base64.b64encode(token).decode('utf-8')
+            request.headers['Authorization'] = (
+                'Negotiate ' + base64.b64encode(token).decode('utf-8')
+            )
         request.register_hook('response', self.handle_401)
         return request
 
@@ -31,30 +33,32 @@ class HTTPNegotiateAuth(AuthBase):
                                    gssapi.C_NT_HOSTBASED_SERVICE)
         return gssapi.InitContext(service_name)
 
-
     authenticate_re = re.compile("""([a-z_\d]+)(=("([^\\"]*(\\.)?)*")|[a-z_\d]*)?(\s+,)?(\s+|$)""", re.I)
+
     def parse_authenticate_header(self, value):
         methods, method, name = {}, None, None
         while value:
-           match = self.authenticate_re.match(value)
-           if not match:
-               return {}
-           if not match.group(6) and match.group(7) is not None:
-               name = match.group(1).title()
-               methods[name] = method = {}
-           else:
-               if match.group(2): # foo=bar
-                   value = match.group(4)
-                   if value.startswith('"'):
-                       value = value[1:-1]
-                   method[match.group(1)] = value.replace(r'\"', '"')
-               else: # it's not really a paramter
-                   methods[name] = match.group(1)
-           value = value[match.end():]
+            match = self.authenticate_re.match(value)
+            if not match:
+                return {}
+            if not match.group(6) and match.group(7) is not None:
+                name = match.group(1).title()
+                methods[name] = method = {}
+            else:
+                if match.group(2):  # foo=bar
+                    value = match.group(4)
+                    if value.startswith('"'):
+                        value = value[1:-1]
+                    method[match.group(1)] = value.replace(r'\"', '"')
+                else:  # it's not really a paramter
+                    methods[name] = match.group(1)
+            value = value[match.end():]
         return methods
 
     def handle_401(self, response, **kwargs):
-        auth_methods = self.parse_authenticate_header(response.headers.get('WWW-Authenticate', ''))
+        auth_methods = self.parse_authenticate_header(
+            response.headers.get('WWW-Authenticate', '')
+        )
         if 'Negotiate' not in auth_methods:
             return response
 
@@ -72,10 +76,11 @@ class HTTPNegotiateAuth(AuthBase):
             if in_token:
                 in_token = base64.b64decode(in_token).decode('utf-8')
             out_token = ctx.step(in_token)
-            prep.headers['Authorization'] = 'Negotiate ' + base64.b64encode(out_token).decode('utf-8')
+            prep.headers['Authorization'] = (
+                'Negotiate ' + base64.b64encode(out_token).decode('utf-8')
+            )
             _r = response.connection.send(prep, **kwargs)
             _r.history.append(response)
             _r.request = prep
 
         return _r
-
